52d4063534e42275ddcb6e53e48df35924b8d0af
manish <ratnawatmanish031@gmail.com>
2025-06-17 04:33:36 +0530

    Intiial commit

---
+++ food-order-back/src/main/resources/import.sql
@@ -0,0 +1,28 @@
+INSERT INTO app_user
+(id, address, deleted, email, first_name, last_name, password, phone_number, role, username)
+VALUES
+(1, 'address line 1', false, 'manish@gmail.com', 'Manish', 'Ratnawat', '123', '060123456', 1, 'manish29329');
+
+INSERT INTO app_user
+(id, address, deleted, email, first_name, last_name, password, phone_number, role, username)
+VALUES
+(2, 'address line 2', false, 'admin@gmail.com', 'John', 'Doe', '123', '0611234321', 0, 'John29329');
+
+INSERT INTO app_user
+(id, address, deleted, email, first_name, last_name, password, phone_number, role, username)
+VALUES
+(3, 'address line 3', false, 'ashish@gmail.com', 'Ashish', 'kumar', '123', '0611234321', 2, 'ashish29329');
+
+--
+--
+--insert into foodorderdb.meal_type (id, type_name) values (1, "PIZZA");
+--insert into foodorderdb.meal_type (id, type_name) values (2, "MAIN COURSE");
+--insert into foodorderdb.meal_type (id, type_name) values (3, "SALAD");
+--insert into foodorderdb.meal_type (id, type_name) values (4, "PANCAKE");
+--insert into foodorderdb.meal_type (id, type_name) values (5, "BURGER");
+--insert into foodorderdb.meal_type (id, type_name) values (6, "PASTA");
+--
+--insert into foodorderdb.meal (id, name, price, meal_type_id) values (1, "CAPRICCIOSA", 600, 1);
+--insert into foodorderdb.meal (id, name, price, meal_type_id) values (2, "SLATKA PALACINKA", 600, 4);
+--insert into foodorderdb.meal (id, name, price, meal_type_id) values (3, "RUSKA SALATA", 600, 3);
+--insert into foodorderdb.meal (id, name, price, meal_type_id) values (4, "NESTOOO", 600, 3);
